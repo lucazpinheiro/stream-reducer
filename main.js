89fe0ded54558cb621505d168aadb8c0b7c151e2
lucas <lucascpinheiro10@gmail.com>
2022-10-20 15:11:37 -0300

    v2 wip

--- main.js
+++ main.js
@@ -2,16 +2,37 @@ import { Readable, Transform } from 'node:stream'
 
 const readable = Readable({
   read() {
+    // const fakeData = [
+    //   '<tag>toy-api</tag>',
+    //   '<tag>node-webwo',
+    //   'rkers-demo</tag>',
+    //   '<tag>points-',
+    //   'of-',
+    //   'interest</tag>',
+    //   '<tag>',
+    //   'processing',
+    //   '-large-',
+    //   'files</tag>',
+    //   '<tag>',
+    //   'todo-app',
+    //   '</tag>',
+    //   '<tag>ts-book</tag>'
+    // ]
     const fakeData = [
-      '<tag>toy-api</tag>',
+      '<othertag><tag>toy-api</tag></othertag><tag>fs</tag>',
       '<tag>node-webwo',
       'rkers-demo</tag>',
       '<tag>points-',
       'of-',
-      'interest</tag>',
-      // '<tag>processing-large-files</tag>',
-      // '<tag>todo-app</tag>',
-      // '<tag>ts-book</tag>'
+      'interest</tag><tag>',
+      'processing',
+      '-large-',
+      'files</tag><tag>nodejs-patterns</tag><tag>concurrency-patterns</tag>',
+      '<tag>v1</tag><tag>v2</tag><tag>v3</tag>',
+      '<tag>',
+      'todo-app',
+      '</tag><tag>ts',
+      '-book</tag><tag>python-api</tag>'
     ]
     fakeData.forEach(str => this.push(str))
     this.push(null)
@@ -29,23 +50,33 @@ let groupedChunks = []
 let holder = Buffer.from('')
 const transform = Transform({
   transform(chunk, encoding, cb) {
+    const data = []
     const tag = 'tag'
 
     groupedChunks.push(chunk)
     holder = Buffer.concat(groupedChunks)
 
-    const [openTagPosition, closeTagPosition] = closingTagsPositions(holder, 'tag')
-    if (openTagPosition > -1 && closeTagPosition > -1) {
+    // const [openTagPosition, closeTagPosition] = closingTagsPositions(holder, 'tag')
+    // if (openTagPosition > -1 && closeTagPosition > -1) {
+    let [openTagPosition, closeTagPosition] = closingTagsPositions(holder, 'tag')
+    while (openTagPosition > -1 && closeTagPosition > -1) {
       groupedChunks.length = 0
-      const data = holder.subarray(openTagPosition, closeTagPosition + (tag.length + 3))
+      data.push(holder.subarray(openTagPosition, closeTagPosition + (tag.length + 3)))
+      // const remainer = holder.subarray(closeTagPosition + (tag.length + 3))
+      // groupedChunks.push(remainer)
+      // holder = Buffer.from('')
       const remainer = holder.subarray(closeTagPosition + (tag.length + 3))
       groupedChunks.push(remainer)
-      holder = Buffer.from('')
+      holder = remainer
 
-      cb(null, data)
+      const values = closingTagsPositions(holder, 'tag')
+      openTagPosition = values[0]
+      closeTagPosition = values[1]
+    }
+    if (data.length) {
+      cb(null, Buffer.from(data))
       return
     }
-
     cb(null, Buffer.alloc(0))
   }
 })
@@ -57,5 +88,6 @@ readable.pipe(transform).on('data', chunk => {
 readable.on('end', _ => {
   console.log('end')
   groupedChunks.forEach(c => console.log(c.toString(), 'in grouped chunks'))
+  console.log(groupedChunks.length)
   console.log(holder.toString(), 'holder')
 })
